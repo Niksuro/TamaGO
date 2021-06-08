8b35791a017d555e77fd04ee61b4ad927ed50c8c
Nicolas Suarez <nicolas.suarez@nivelics.co>
2021-06-07 19:41:09 -0500

    MenuComiuda

--- Assets/Script/Open_Menu.cs
+++ Assets/Script/Open_Menu.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class Open_Menu : MonoBehaviour {
 
-	Animator Anim_Menu, Anim_Btn, Anim_Config;
-	public GameObject Menu, Btn, Config_btn;
+	Animator Anim_Menu, Anim_Btn, Anim_Config, Anim_foodBtn, Anim_foodMenu;
+	public GameObject Menu, Btn, Config_btn, food_btn, Food_Menu;
 	// Use this for initialization
 	void Start (){
 		Anim_Menu = Menu.GetComponent<Animator>();
 		Anim_Btn = Btn.GetComponent<Animator>();
 		Anim_Config = Config_btn.GetComponent<Animator>();
+		Anim_foodBtn = food_btn.GetComponent<Animator>();
+		Anim_foodMenu = Food_Menu.GetComponent<Animator>();
 	}
 	public void Switch_menu () {
 		Anim_Menu.SetTrigger("Open");
@@ -29,6 +31,7 @@ public class Open_Menu : MonoBehaviour {
 
 	public void Switch_Food_Menu()
     {
-
+		Anim_foodBtn.SetTrigger("Open");
+		Anim_foodMenu.SetTrigger("Open");
     }
 }
